5d0312e8db752b2376ec1229b7a0264fb4db17a8
Gal Afik <gafik@quotient-inc.local>
2017-01-18 15:06:20 -0500

    added roni base site

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Gal Afik &middot; Java Programmer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
+    <!--[if gte IE 9]>
+      <style type="text/css">
+        .gradient {
+           filter: none;
+        }
+      </style>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="ico/favicon.png">
+
+
+    <link href="http://fonts.googleapis.com/css?family=Corben:bold" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Nobile" rel="stylesheet" type="text/css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container-narrow">
+
+      <div class="masthead">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Welcome</a></li>
+          <li><a href="#marketing">Skills</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <h3 class="muted">Gal Phillip Afik</h3>
+      </div>
+
+      <hr/>
+
+      <div class="jumbotron">
+        <h1>Java Engineer</h1>
+        <p class="lead">Seeking a full-time position in the tech industry starting May 2016. For a full list of skills and qualifications, download my resume below.</p>
+        <a class="btn btn-large btn-success btn-custom" href="pdf/resume.pdf" target="_blank"><i class="icon-file icon-white"></i>Download</a>
+      </div>
+
+      <hr/>
+
+      <div class="row-fluid">
+        <div class="about span5">
+          <h4>About me</h4><p>I am a senior at the University of Maryland, Baltimore County and have more than nine years of programming experience.</p>
+        </div>
+        <div class="span2" align="center"> <img alt="Gal Afik" src="img/headshot.jpg" class="img-circle"> </div>
+        <div class="about span5">
+          <h4>Info</h4><p>9970 Guilford Rd. Jessup, MD 20794</p><p>(240)-388-3510</p><p>afik1@umbc.edu</p><p>afikdesigns@gmail.com</p>
+        </div>
+      </div>
+
+      <hr/>
+
+      <div class="row-fluid">
+        <div class="about span6">
+          <img alt="HackUMBC" src="img/mlh.png">
+        </div>
+        <div class="span6" align="center"><p><h3>Top 10</h3><h4> HackUMBC 2014</h4></div>
+      </div>
+
+      <hr/>
+
+      <div class="row-fluid marketing" id="marketing">
+        <div class="span6">
+          <h4>Back-End</h4>
+          <p>With more than 20 programming languages under my belt and a wide range of computer science skills, I can offer high-quality software solutions.</p>
+
+          <h4>Front-End</h4>
+          <p>I have two years of design and development experience and am fluent in HTML5, CSS3, Javascript, and MySQL technologies.</p>
+
+        </div>
+
+        <div class="span6">
+          <h4>Hardware</h4>
+          <p>I have professional experience imaging, setting-up, and deploying hardware and software solutions within an IT setting for corporate and off-site locations.</p>
+
+          <h4>Client-side</h4>
+          <p>I have worked with clients and customers both online, over the phone, and in person and have the people skills to prove it.</p>
+
+        </div>
+      </div>
+
+      <hr/>
+
+      <div class="row-fluid">
+        <div class="about span6">
+          <img alt="Oracle Certified Associate" src="img/oca.gif">
+        </div>
+        <div class="span6" align="center"><p><h3>OCA</h3><h4> January 2016</h4></div>
+      </div>
+
+      <hr/>
+
+      <div class="timeline">
+        <h1>Experience</h1>
+        <div class="well">
+          <div class="row-fluid">
+            <img class="span3" src="img/quotient.png">
+            <div class="span6">
+              <h4>Jr. Developer</h4>
+            </div>
+            <h5>2016</h5>
+          </div>
+        </div>
+        <div class="well">
+          <div class="row-fluid">
+            <img class="span3" src="img/caci.png">
+            <div class="span6">
+              <h4>Software Design Intern</h4>
+              <p>Developed an integrated server system for data extraction and filtering</p>
+            </div>
+            <h5>2015</h5>
+          </div>
+        </div>
+        <div class="well">
+          <div class="row-fluid">
+            <img class="span3" src="img/bac.png">
+            <div class="span6">
+              <h4>IT Helpdesk Intern</h4>
+              <p>Set up and deployed hardware components within the corporate office and in the MD fatory</p>
+            </div>
+            <h5>2014</h5>
+          </div>
+        </div>
+        <div class="well">
+          <div class="row-fluid">
+            <img class="span3" src="img/ecompex.png">
+            <div class="span6">
+              <h4>Software Development Consultant</h4>
+              <p>Designed a PTO Tracking system for internal company-wide use</p>
+            </div>
+            <h5>2014</h5>
+          </div>
+        </div>
+        <div class="well">
+          <div class="row-fluid">
+            <img class="span3" src="img/mindgrub.png">
+            <div class="span6">
+              <h4>Web Developer Intern</h4>
+              <p>Build web apps for industry leaders such as Snyder's Pretzels and Polk Audio</p>
+            </div>
+            <h5>2013</h5>
+          </div>
+        </div>
+        <div class="well">
+          <div class="row-fluid">
+            <img class="span3" src="img/visarts.png">
+            <div class="span6">
+              <h4>Web Design Intern</h4>
+              <p>Worked hands-on with clients in order to build unique websites</p>
+            </div>
+            <h5>2009</h5>
+          </div>
+        </div>
+      </div>
+
+      <hr/>
+
+      <div class="logos">
+        <h1>Design Work</h1>
+        <div class="row-fluid">
+          <img src="img/1.png" width="250" height="250"> 
+          <img src="img/2.png" width="250" height="250"> 
+          <img src="img/3.png" width="250" height="250"> 
+          <img src="img/4.png" width="250" height="250"> 
+          <img src="img/5.png" width="250" height="250"> 
+          <img src="img/6.png" width="250" height="250"> 
+          <img src="img/7.png" width="250" height="250"> 
+          <img src="img/8.png" width="250" height="250">
+        </div>
+      </div>
+
+      <hr/>
+
+      <div class="footer">
+        <div class="pull-right">
+          <a href="https://www.facebook.com/gal.afik.9" target="_blank"><i id="social" class="fa fa-facebook-square fa-3x social-fb"></i></a>
+          <a href="https://www.linkedin.com/in/galafik" target="_blank"><i id="social" class="fa fa-linkedin-square fa-3x social-li"></i></a>
+          <a href="https://twitter.com/GalAfik" target="_blank"><i id="social" class="fa fa-twitter-square fa-3x social-tw"></i></a>
+          <a href="https://plus.google.com/112212856064297676539" target="_blank"><i id="social" class="fa fa-google-plus-square fa-3x social-gp"></i></a>
+        </div>
+        <p>&copy; Afik Web Designs 2013-2016</p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
+
+<script>
+$(document).ready(function () {
+    $('#myCarousel').carousel({
+        interval: 10000
+    })
+    $('.fdi-Carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+});
+</script>
+
